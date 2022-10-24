19407a2b575a4faf92d5ed616365ec186b4eb760
Thiago Wlasenko Nicolau <thi_wn@hotmail.com>
2022-10-24 15:25:59 -0300

    Initial Commit

---
+++ TestPackageApp/TestPackageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPackageAppApp.swift
+//  TestPackageApp
+//
+//  Created by Thiago  Wlasenko Nicolau on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TestPackageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
