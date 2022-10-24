a21b8f6b862e3923ff4a6862c0c3b5f5492347e8
Thiago Wlasenko Nicolau <thi_wn@hotmail.com>
2022-10-24 16:29:57 -0300

    Adding configure to run package example

--- TestPackageApp/TestPackageAppApp.swift
+++ TestPackageApp/TestPackageAppApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import PackageExample
 
 @main
 struct TestPackageAppApp: App {
+    
+    init() {
+        PackageExample().configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
